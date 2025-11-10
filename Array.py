8b186c167c98e377a9e79ae9de47215d0750fbfa
Dermawan-25 <dermawanmawan444@gmail.com>
2025-11-10 15:59:56 +0800

    Create Array.py

---
+++ Array.py
@@ -0,0 +1,20 @@
+# NAMA : DARMAWAN 
+# NIM : D0425319
+# SISTEM INFORMASI B
+# ARRAY
+
+Makanan = ["somay", "pentol", "sempol", "naget", "ayam penyet"]
+
+# Menampilkan semua nama
+print("Daftar Makanan:", Makanan)
+
+# Mengakses 
+print("Makanan pertama:", Makanan[0])
+
+# Menambah data 
+Makanan.append("sate ayam")
+print("Setelah ditambah:", Makanan)
+
+# Menghapus 
+Makanan.remove("somay")
+print("Setelah somay dihapus:", Makanan)
